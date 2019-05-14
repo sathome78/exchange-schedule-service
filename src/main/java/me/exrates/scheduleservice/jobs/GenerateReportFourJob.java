9652a7a5923d39386bb7a20ac8fbceb2674e578b
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-05-14 14:20:31 +0300

    DEVEX-2048 (add logs to jobs)

--- src/main/java/me/exrates/scheduleservice/jobs/GenerateReportFourJob.java
+++ src/main/java/me/exrates/scheduleservice/jobs/GenerateReportFourJob.java
@@ -1,5 +1,6 @@
 package me.exrates.scheduleservice.jobs;
 
+import lombok.extern.log4j.Log4j2;
 import me.exrates.scheduleservice.services.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +8,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @EnableScheduling
+@Log4j2(topic = "Cron_job_layer_log")
 @Component
 public class GenerateReportFourJob {
 
